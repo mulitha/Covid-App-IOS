dcd59a9d2cbf04761a7cfad0b5e861438b29d64e
mulitha <mulitha@mulithas-MacBook-Pro.local>
2020-09-20 11:46:53 -0700

    contact us icon added

--- NIBM COVID19/Controllers/SettingsViewController.swift
+++ NIBM COVID19/Controllers/SettingsViewController.swift
@@ -17,40 +17,40 @@ class SettingsViewController: UITableViewController  {
     
 
 
-    func navigateToSignUp(){
-        
-        DispatchQueue.main.async {
-                                    
-            let mianStorybord = UIStoryboard(name:"Main", bundle: Bundle.main)
-            guard let signInVC = mianStorybord.instantiateViewController(withIdentifier: "LoginViewController") as?
-                LoginViewController else{
-                    return
-            }
-            
-            let navigation = UINavigationController(rootViewController: signInVC)
-            navigation.modalPresentationStyle = .fullScreen
-            self.present(navigation,animated: true,completion: nil)
-
-        }
-        
-        
-    }
+//    func navigateToSignUp(){
+//
+//        DispatchQueue.main.async {
+//
+//            let mianStorybord = UIStoryboard(name:"Main", bundle: Bundle.main)
+//            guard let signInVC = mianStorybord.instantiateViewController(withIdentifier: "LoginViewController") as?
+//                LoginViewController else{
+//                    return
+//            }
+//
+//            let navigation = UINavigationController(rootViewController: signInVC)
+//            navigation.modalPresentationStyle = .fullScreen
+//            self.present(navigation,animated: true,completion: nil)
+//
+//        }
+//
+//
+//    }
     
     
     
-       override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            if(Service.shared.getUserUid() == "") {
-                if indexPath.row == 0 {
-                    return 0
-                }
-            }else{
-                if indexPath.row == 1 {
-                    return 0
-                }
-            }
-        return tableView.rowHeight
-    
-    }
+//       override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//            if(Service.shared.getUserUid() == "") {
+//                if indexPath.row == 0 {
+//                    return 0
+//                }
+//            }else{
+//                if indexPath.row == 1 {
+//                    return 0
+//                }
+//            }
+//        return tableView.rowHeight
+//
+//    }
     
   
 
